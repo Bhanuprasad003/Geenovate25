cfa0596802a77d340254bce136a33aed6e139f22
Bhanuprasad003 <bhanuprasadchunchu03@gmail.com>
2025-04-26 09:02:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -190,6 +190,6 @@ def process_pdf():
         }), 500
 
 
-if _name_ == "_main_":
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))  # Render sets this
     app.run(host="0.0.0.0", port=port)
